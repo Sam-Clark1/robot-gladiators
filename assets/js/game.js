a60cac41563f4a3d5d45b4237dd378b3c1d1be14
Sam Clark <samclark2399@gmail.com>
2022-01-19 19:43:28 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,13 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
@@ -27,19 +34,20 @@ var fight = function(enemyName) {
 
       // if yes (true), leave fight
       if (confirmSkip) {
-        window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+        window.alert(playerName + ' has decided to skip this fight.');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
-    );
+    console.log(enemyHealth, enemyAttack)
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
+    console.log(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
+    console.log(enemyHealth, enemyAttack)
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -55,10 +63,11 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
-      enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
-    );
+    console.log(playerHealth, playerAttack)
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
+    console.log(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
+    console.log(playerHealth, playerAttack)
 
     // check player's health
     if (playerHealth <= 0) {
@@ -82,7 +91,7 @@ var fight = function(enemyName) {
       if (playerHealth > 0) {
         window.alert(`Welcome to Robot Gladiators! Round ${i + 1}`)
         var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50
+        enemyHealth = randomNumber(40, 60);
         fight(pickedEnemyName);
       }
       if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -96,7 +105,6 @@ var fight = function(enemyName) {
         break;
       }
     }
-
     // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
     endGame();
   };  
@@ -139,8 +147,8 @@ var shop = function() {
       if (playerMoney >= refillCost) {
         
           window.alert(`Refilling player's health by 20 for ${refillCost} dollars.`);
-          playerHealth = playerHealth + 20;
-          playerMoney = playerMoney - refillCost;
+          playerHealth = 0, playerHealth + 20;
+          playerMoney = Math.max(0, playerMoney - refillCost);
           console.log("Health = " + playerHealth)
           console.log("Money = " + playerMoney)
           break;
@@ -156,7 +164,7 @@ var shop = function() {
       if (playerMoney >= upgradeCost) {
           window.alert(`Upgrading player's attack by 6 for ${upgradeCost} dollars.`);
           playerAttack = playerAttack + 6;
-          playerMoney = playerMoney - upgradeCost;
+          playerMoney = Math.max(0, playerMoney - upgradeCost);
           console.log("Attack = " + playerAttack)
           console.log("Money = " + playerMoney)
           break;
@@ -178,6 +186,8 @@ var shop = function() {
       break;
   }
 }
+
+
 
 // start the game when the page loads
 startGame();
